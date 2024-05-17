bc26a29005f14c5b9dfb972cc4766bdc8b63c4d1
gialbanes <giovanaalbanes@gmail.com>
2024-05-17 11:45:46 -0300

    arrumei medidas e ainda não ta responsivo

---
+++ views/js/script.js
@@ -0,0 +1,63 @@
+window.sr = ScrollReveal({ reset: true });
+
+
+/*PARTE INICIAL*/
+sr.reveal('ul',{
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 800,  
+});
+
+sr.reveal('#home',{
+    rotate: {x: 0, y: 10, z: 0},
+    duration: 700,
+});
+/*PARTE INICIAL*/
+
+
+
+sr.reveal('#sobre',{
+    rotate: {x: 20, y: 0, z: 0},
+    duration: 1000,
+});
+
+
+sr.reveal('#quemsomos',{
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 1000,
+});
+
+
+
+sr.reveal('#projeto',{
+    rotate: {x: 80, y: 0, z: 0},
+    duration: 1000,
+});
+
+
+/*EQUIPE*/
+sr.reveal('#membros',{
+    rotate: {x: 0, y: 80, z: 80},
+    duration: 1000,
+    
+});
+
+sr.reveal('#membrosefeitos',{
+    rotate: {x: 0, y: 80, z: 80},
+    duration: 1000,
+    delay: 200
+});
+
+sr.reveal('#fundo',{
+    rotate: {x: 80, y: 0, z: 80},
+    duration: 1000,
+    delay: 300
+});
+
+
+/*EQUIPE*/
+
+
+sr.reveal('footer',{
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 1000,
+});
